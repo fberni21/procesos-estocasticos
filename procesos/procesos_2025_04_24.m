3f95576939bc1a6b537855589794d7d78d80c1b6
Franco Berni <fberni@fi.uba.ar>
2025-05-03 11:46:42 -0300

    Finish ejercicios 2025-04-24

--- procesos/procesos_2025_04_24.m
+++ procesos/procesos_2025_04_24.m
@@ -21,7 +21,7 @@ Z = rand(M, N) < p;
 X = 2*Z - 1;
 
 mu_X = mean(X)
-var_X = var(X);
+var_X = var(X)
 
 figure();
 
@@ -88,3 +88,184 @@ xlabel('t / s');
 ylabel('\sigma^2(t)');
 legend(['Varianza estimada'; 'Varianza teórica']);
 title('Varianza del proceso');
+
+% Actividad 3
+
+rand('seed', 42);
+
+M = 20;
+N = 50;
+
+n = 0:N;
+
+probs = [0.2 0.5 0.8];
+
+for p = probs
+  Z = rand(M, N) < p;
+
+  X = 2*Z - 1;
+
+  Y = [zeros(M, 1) cumsum(X, 2)];
+
+  mu_Y_exact = n * (2 * p - 1);
+  var_Y_exact = n * 4 * p * (1 - p);
+
+  mu_Y = mean(Y);
+  var_Y = var(Y);
+
+  figure();
+  subplot(2, 1, 1);
+  stairs(n, Y', 'color', 'b');
+  hold on;
+  h1 = plot(n, mu_Y, 'linewidth', 4, 'color', 'r');
+  h2 = plot(n, mu_Y_exact, '--', 'color', 'k', 'linewidth', 3);
+  title(['Realizaciones y media del proceso para p = ' num2str(p)]);
+  xlabel('n');
+  ylabel('Y(n)');
+  legend([h1 h2], ['Media estimada'; 'Media teórica']);
+
+  subplot(2, 1, 2);
+  plot(n, var_Y);
+  hold on;
+  plot(n, var_Y_exact, '--');
+  xlabel('n');
+  ylabel('\sigma^2(n)');
+  legend(['Varianza estimada'; 'Varianza teórica']);
+  title('Varianza del proceso');
+endfor
+
+% Actividad 4
+
+rand('seed', 42);
+
+N = 400;
+M = 20;
+
+omega = 0.015*pi;
+
+mu_A = 1;
+var_A = 0.16;
+
+A = normrnd(mu_A, sqrt(var_A), M, 1);
+
+n = 0:N-1;
+
+X = A * cos(omega * n);
+
+mu_X = cos(omega * n);
+
+mu_X_est = mean(X);
+
+figure();
+plot(n, X', 'color', 'b');
+hold on;
+h1 = plot(n, mu_X_est, 'linewidth', 4, 'color', 'r');
+h2 = plot(n, mu_X, '--', 'color', 'k', 'linewidth', 3);
+title('Realizaciones y media del proceso');
+xlabel('n');
+ylabel('X(n)');
+legend([h1 h2], ['Media estimada'; 'Media teórica']);
+grid on;
+
+M = 200;
+
+A = normrnd(mu_A, sqrt(var_A), M, 1);
+
+n = 0:N-1;
+
+X = A * cos(omega * n);
+
+mu_X = cos(omega * n);
+var_X = var_A * cos(omega * n).^2;
+
+mu_X_est = mean(X);
+var_X_est = var(X);
+
+figure();
+subplot(1, 2, 1);
+plot(n, mu_X_est, 'linewidth', 2, 'color', 'r');
+hold on;
+plot(n, mu_X, '--', 'color', 'k', 'linewidth', 2);
+title('Media del proceso');
+xlabel('n');
+ylabel('\mu_X(n)');
+legend(['Media estimada'; 'Media teórica']);
+grid on;
+
+subplot(1, 2, 2);
+plot(n, var_X_est, 'linewidth', 2, 'color', 'r');
+hold on;
+plot(n, var_X, '--', 'color', 'k', 'linewidth', 2);
+title('Varianza del proceso');
+xlabel('n');
+ylabel('\sigma^2_X(n)');
+legend(['Varianza estimada'; 'Varianza teórica']);
+grid on;
+
+% Actividad 5
+
+N = 400;
+M = 20;
+
+omega = 0.015 * pi;
+
+theta = unifrnd(0, 2*pi, M, 1);
+
+n = 0:N-1;
+
+A = 1;
+
+X = A * cos(omega * n + theta);
+
+mu_X = zeros(N, 1);
+
+mu_X_est = mean(X);
+
+figure();
+plot(n, X', 'color', 'b');
+hold on;
+h1 = plot(n, mu_X_est, 'linewidth', 4, 'color', 'r');
+h2 = plot(n, mu_X, '--', 'color', 'k', 'linewidth', 3);
+title('Realizaciones y media del proceso');
+xlabel('n');
+ylabel('X(n)');
+legend([h1 h2], ['Media estimada'; 'Media teórica']);
+ylim([-1 1]);
+grid on;
+
+M = 2000;
+
+theta = unifrnd(0, 2*pi, M, 1);
+
+n = 0:N-1;
+
+X = A * cos(omega * n + theta);
+
+mu_X = zeros(N, 1);
+var_X = ones(N, 1) * (A^2 / 2);
+
+mu_X_est = mean(X);
+var_X_est = var(X);
+
+figure();
+subplot(1, 2, 1);
+plot(n, mu_X_est, 'linewidth', 2, 'color', 'r');
+hold on;
+plot(n, mu_X, '--', 'color', 'k', 'linewidth', 2);
+title('Media del proceso');
+xlabel('n');
+ylabel('\mu_X(n)');
+legend(['Media estimada'; 'Media teórica']);
+ylim([-1 1]);
+grid on;
+
+subplot(1, 2, 2);
+plot(n, var_X_est, 'linewidth', 2, 'color', 'r');
+hold on;
+plot(n, var_X, '--', 'color', 'k', 'linewidth', 2);
+title('Varianza del proceso');
+xlabel('n');
+ylabel('\sigma^2_X(n)');
+legend(['Varianza estimada'; 'Varianza teórica']);
+ylim([0 1]);
+grid on;
